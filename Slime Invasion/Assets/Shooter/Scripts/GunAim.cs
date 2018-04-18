0094edb4894af627bac3feff4076d57e2aec87be
PseudoHyena <anzen.bagnyuk@gmail.com>
2018-04-18 20:44:37 +0300

    Improve showing health bar of slimes
    It's seen when player look at it

---
+++ Slime Invasion/Assets/Shooter/Scripts/GunAim.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GunAim : MonoBehaviour {
+
+    [SerializeField] float ShowHealthRange = 50f;
+
+    Camera cam;
+    GameObject lastSlimeCanvas;
+
+    void Start() {
+        cam = Camera.main;    
+    }
+
+    void Update () {
+        ShowHealthOfSlime();
+	}
+
+    void ShowHealthOfSlime() {
+        Vector3 rayOrigin = cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+        RaycastHit hit;
+        Ray ray = new Ray(rayOrigin, cam.transform.forward);
+
+        if (Physics.Raycast(ray, out hit, ShowHealthRange)) {
+            if (hit.collider.tag == "Slime") {
+                lastSlimeCanvas = hit.collider.GetComponentInChildren<Canvas>(true).gameObject;
+                lastSlimeCanvas.SetActive(true);
+            }
+        }
+        else {
+            if (lastSlimeCanvas != null) {
+                lastSlimeCanvas.SetActive(false);
+            }
+        }
+    }
+}
