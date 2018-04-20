20cee9e286a902a2c10f43a71547094a45d59fc7
PseudoHyena <anzen.bagnyuk@gmail.com>
2018-04-20 21:11:49 +0300

    Add water impact on player

--- Slime Invasion/Assets/Player/Scripts/Player.cs
+++ Slime Invasion/Assets/Player/Scripts/Player.cs
@@ -4,14 +4,30 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class Player : MonoBehaviour, IDamageable {
 
+    struct MovementPresset {
+        public float ForwardSpeed;
+        public float BackwardSpeed;
+        public float StrafeSpeed;
+        public float RunMultiplier;
+        public float JumpForce;
+    }
+
     [SerializeField] int maxHealth = 100;
     [SerializeField] Slider healthSlider;
+    [SerializeField] Slider airSlider;
 
     float health;
 
     float waterHeight;
+    float canBreathSec = 20f;
+    float startDive;
+    bool isUnderWater = false;
 
     GameManager manager;
+
+    MovementPresset movementPresset;
+    RigidbodyFirstPersonController controller;
+    CapsuleCollider capsuleCollider;
 
     void Start() {
         health = maxHealth;
@@ -20,12 +36,27 @@ public class Player : MonoBehaviour, IDamageable {
 
         manager = FindObjectOfType<GameManager>();
 
+        controller = GetComponent<RigidbodyFirstPersonController>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
         waterHeight = GameManager.WaterHeight;
+        airSlider.maxValue = canBreathSec;
+        airSlider.value = canBreathSec;
+
+        SetMovementPresset();
     }
 
     void Update() {
         CheckForOutOfMap();
-        CheckForUnderWater();
+        CheckForWaterImpact();
+    }
+
+    void SetMovementPresset() {
+        movementPresset.ForwardSpeed = controller.movementSettings.ForwardSpeed;
+        movementPresset.BackwardSpeed = controller.movementSettings.BackwardSpeed;
+        movementPresset.StrafeSpeed = controller.movementSettings.StrafeSpeed;
+        movementPresset.RunMultiplier = controller.movementSettings.RunMultiplier;
+        movementPresset.JumpForce = controller.movementSettings.JumpForce;
     }
 
     public void TakeDamage(int damage) {
@@ -46,9 +77,40 @@ public class Player : MonoBehaviour, IDamageable {
         }
     }
 
-    void CheckForUnderWater() {
-        if (transform.position.y < waterHeight) {
-            Debug.Log("Water!");
+    void CheckForWaterImpact() {
+        if (transform.position.y - capsuleCollider.height / 2f + 0.1f < waterHeight) {
+            controller.movementSettings.ForwardSpeed = movementPresset.ForwardSpeed / 2.5f;
+            controller.movementSettings.BackwardSpeed = movementPresset.BackwardSpeed / 2.5f;
+            controller.movementSettings.StrafeSpeed = movementPresset.StrafeSpeed / 2.5f;
+            controller.movementSettings.RunMultiplier = 1.1f;
+            controller.movementSettings.JumpForce = movementPresset.JumpForce / 2.5f;
+        }
+        else {
+            controller.movementSettings.ForwardSpeed = movementPresset.ForwardSpeed;
+            controller.movementSettings.BackwardSpeed = movementPresset.BackwardSpeed;
+            controller.movementSettings.StrafeSpeed = movementPresset.StrafeSpeed;
+            controller.movementSettings.RunMultiplier = movementPresset.RunMultiplier;
+            controller.movementSettings.JumpForce = movementPresset.JumpForce;
+        }
+
+        if (transform.position.y + capsuleCollider.height / 2f + 0.1f < waterHeight) {
+            if (!isUnderWater) {
+                isUnderWater = true;
+                startDive = Time.time;
+                airSlider.gameObject.SetActive(true);
+            }
+        }
+        else {
+            isUnderWater = false;
+            airSlider.gameObject.SetActive(false);
+            airSlider.value = canBreathSec;
+        }
+
+        if (isUnderWater) {
+            airSlider.value = startDive + canBreathSec - Time.time;
+            if (Time.time > startDive + canBreathSec) {
+                Die();
+            }
         }
     }
 
